663b9b9535db0dac20870bd36a9726bc3ea34b71
SushantThakare <168189427+SushantThakare@users.noreply.github.com>
2024-04-26 17:05:34 +0530

    Add files via upload

---
+++ 2_Opraters/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Opraters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10, j = 20;
+            
+            Console.WriteLine(i++);
+            Console.WriteLine(i);
+            Console.WriteLine(++i);
+            i += 5;
+            Console.WriteLine(i);
+            i -= 7;
+            Console.WriteLine(i);
+            i /= 2;
+            Console.WriteLine(i);
+            bool result = i == j;
+            Console.WriteLine(result);
+            bool result2 = i != j;
+            Console.WriteLine(result2);
+
+            result =i<j;
+            Console.WriteLine(result);
+            result = i > j;
+                Console.WriteLine(result);
+          
+            Console.ReadLine();
+        }
+    }
+}
