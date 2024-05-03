ae03248c32bb049207204b34d12d9f37030185f1
SushantThakare <168189427+SushantThakare@users.noreply.github.com>
2024-05-03 11:16:50 +0530

    Nullable Type in C#

---
+++ 6_Nullable/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_Nullable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Sushant";
+            Console.WriteLine(name);
+            name = null;
+            Console.WriteLine(name);
+            int? age = null;
+            bool? b = null;
+
+            //int? a = 10;
+            //int b1 = (int)a;
+            //Console.WriteLine($" a:{a} b : {b1}");
+
+            int? a = null;
+            int b1 = a ?? 50;
+            Console.WriteLine($" a:{a} b : {b1}");
+
+            string email = "Sushant@gmail.com";
+            string emailToUpper = email.ToUpper();
+            Console.WriteLine($"Email {email} emailToUpper : {emailToUpper} \n " );
+
+            string Email = null;
+            string EmailToUpper = Email?.ToUpper();
+            Console.WriteLine($"Email {Email} emailToUpper : {EmailToUpper}");
+            Console.ReadLine();
+        }
+    }
+}
