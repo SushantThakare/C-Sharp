a172cdc623ca208c0a9310729253a26ca2765126
SushantThakare <168189427+SushantThakare@users.noreply.github.com>
2024-07-11 11:43:02 +0530

    optional parameter

---
+++ 14_OptionalParameter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_OptionalParameter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //add(30, 70);
+
+            add(b:50);
+
+            sum(10, c:10);
+            Console.ReadLine();
+        }
+
+        public static void add([Optional] int a,int b)
+        {
+            Console.WriteLine($"{a} + {b} = {a + b}");
+        }
+
+        public static void sum (int a , [Optional]int b, int c)
+        {
+            Console.WriteLine($"{a} + {b} + {c} = {a+b+c}");
+        }
+    }
+}
