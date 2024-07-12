7ba479160e94df4d1c6e71e314f5c51a98b3e1a0
SushantThakare <168189427+SushantThakare@users.noreply.github.com>
2024-07-12 11:50:55 +0530

    Boxing Unboxing

---
+++ 15_BoxingUnboxing/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15_BoxingUnboxing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+            Console.WriteLine(i);
+            //Boxing
+            object obj = i;
+            Console.WriteLine(obj);
+            //UnBoxing
+            int j = (int)obj;
+            Console.WriteLine(j);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
