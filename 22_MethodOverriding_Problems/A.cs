1f66f95f3275782e20f965f708178c4308e6a5a8
SushantThakare <168189427+SushantThakare@users.noreply.github.com>
2024-07-22 12:40:26 +0530

    Method Overriding

---
+++ 22_MethodOverriding_Problems/A.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22_MethodOverriding_Problems
+{
+    public class A
+    {
+        public virtual void Print()
+        {
+            Console.WriteLine("A");
+        }
+    }
+
+    public  class B:A
+    {
+        public override void Print()
+        {
+            Console.WriteLine("B");
+        }
+    }
+    public  class C:B
+    {
+        public  void Print()
+        {
+            Console.WriteLine("C");
+        }
+    }
+}
---
+++ 22_MethodOverriding_Problems/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22_MethodOverriding_Problems
+{
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                A a=new A(); a.Print();
+                A a1=new B(); a1.Print();
+                A a2=new C(); a2.Print();//late binding of virtual
+                B b=new C(); b.Print();
+                Console.ReadLine();
+            }
+        }
+}
