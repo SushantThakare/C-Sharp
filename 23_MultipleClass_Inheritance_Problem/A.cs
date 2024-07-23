275cdddba8373fe08690e3ff736e0c4a57387f60
SushantThakare <168189427+SushantThakare@users.noreply.github.com>
2024-07-23 11:24:13 +0530

    Multiple Class Inheritance Problem

---
+++ 23_MultipleClass_Inheritance_Problem/A.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23_MultipleClass_Inheritance_Problem
+{
+    public class A
+    {
+        public virtual void print()
+        {
+            Console.WriteLine("A");
+        }
+    }
+
+    public class B : A
+    {
+        public override void print()
+        {
+            Console.WriteLine("B");
+        }
+    }
+
+    public class C : A
+    {
+        public override void print()
+        {
+            Console.WriteLine("C");
+        }
+    }
+
+    public class D : B
+    {
+      
+    }
+}
---
+++ 23_MultipleClass_Inheritance_Problem/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23_MultipleClass_Inheritance_Problem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new A();a.print();
+            B b = new B();b.print();
+            C c = new C();c.print();
+            D d = new D();d.print();
+           
+
+            Console.ReadLine();
+        }
+    }
+}
