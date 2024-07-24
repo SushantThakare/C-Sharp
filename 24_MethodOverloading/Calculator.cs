56eefbdd58ae185111f3c17a13943e9f51122646
SushantThakare <168189427+SushantThakare@users.noreply.github.com>
2024-07-24 11:00:58 +0530

    Method Overloading

---
+++ 24_MethodOverloading/Calculator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24_MethodOverloading
+{
+   public class Calculator
+    {
+        public void print()
+        {
+            Console.WriteLine("print() called");
+        }
+        //public int print()//not possible 
+        //{
+        //    Console.WriteLine("print() called");
+        //    return 10;
+        //}
+        public void print(int num)
+        {
+            Console.WriteLine("print(int num) called");
+        }
+
+        public void print(short num)
+        {
+            Console.WriteLine("print(short num) called");
+        }
+
+        public void print(ref int num)
+        {
+            Console.WriteLine("print(ref int num) called");
+        }
+
+        public void print(int a,int b,out int result)
+        {
+            result = a * b;   
+            Console.WriteLine($"print(int a,int b,out int result) called. result:- {result}");
+        }
+
+
+        public void print(int num1,int num2)
+        {
+            Console.WriteLine("print(int num1,int num2) called");
+        }
+        public void print(string name)
+        {
+            Console.WriteLine("print(string name) called");
+        }
+        public void print(string name,int num)
+        {
+            Console.WriteLine("print(string name,int num) called");
+        }
+        public void print(int num,string name)
+        {
+            Console.WriteLine("print(int num,string name) called");
+        }
+
+    }
+}
---
+++ 24_MethodOverloading/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24_MethodOverloading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator cal = new Calculator();
+            cal.print();
+            cal.print(10);
+            int i = 20;
+            cal.print(ref i);
+            int result;
+            cal.print(50,2,out result);
+            cal.print(10,20);
+            cal.print("Sushant");
+            cal.print("Sushant",1);
+            cal.print(1,"Sushant");
+
+            short s = 10;
+            int i1 = 10;
+            cal.print(10);
+            cal.print(s);
+            Console.ReadLine();
+        }
+    }
+}
