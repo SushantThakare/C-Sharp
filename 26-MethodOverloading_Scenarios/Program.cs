dea4bdde556fdbca9feb6c0613a38a5aaccf4bf0
SushantThakare <168189427+SushantThakare@users.noreply.github.com>
2024-07-29 13:00:16 +0530

    Method Overloading Scenarios

---
+++ 26-MethodOverloading_Scenarios/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _26_MethodOverloading_Scenarios
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new A();
+            a.Print();
+            B b = new B();
+            b.Print(10);
+            b.Print();
+            
+            Console.ReadLine();
+
+            
+        }
+    }
+
+    public class A
+    {
+        public void Print()
+        {
+            Console.WriteLine("Print() Printed");
+        }
+    }
+
+    public class B:A
+    {
+        public void Print(int a)
+        {
+            Console.WriteLine($"{a}:Print(int a) Printed");
+        }
+    }
+}
