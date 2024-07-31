17d2cbd687d51e9560ee25e2577f7904ba98f938
SushantThakare <168189427+SushantThakare@users.noreply.github.com>
2024-07-31 11:24:03 +0530

    Sealed Keyword

---
+++ 28-SealedKeyword/Customer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28_SealedKeyword
+{
+    public  class Customer
+    {
+        public virtual void print()
+        {
+            Console.WriteLine("Customer print() called");
+        }
+    }
+
+    public class Silver:Customer
+    {
+        public override void print()
+        {
+           Console.WriteLine("Silver print() called");
+        }
+       
+    }
+
+    public class AdvancedSilver:Silver
+    {
+        public override void print()
+        {
+
+            Console.WriteLine("AdvancedSilver print() called");
+        }
+    }
+}
---
+++ 28-SealedKeyword/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28_SealedKeyword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer c1 = new Customer();
+            c1.print();
+            Customer c2 = new Silver();
+            c2.print();
+            Customer c3 = new AdvancedSilver();
+            c3.print();
+            Console.ReadLine();
+        }
+    }
+
+}
