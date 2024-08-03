f76fa2a0b7b7c44f3a1da618c8d9018cc112fe7c
SushantThakare <168189427+SushantThakare@users.noreply.github.com>
2024-08-03 15:48:07 +0530

    Math Library Client2

---
+++ 32-MathLibraryClient2/Program.cs
@@ -0,0 +1,21 @@
+﻿using _30_MathLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _32_MathLibraryClient2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To my Calculator App");
+            Calculator c1 = new Calculator();
+            c1.add(10,20);
+            Console.WriteLine("Thankyou for using calculator app");
+            Console.ReadLine();
+        }
+    }
+}
