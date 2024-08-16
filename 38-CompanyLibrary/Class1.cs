e0668fbd608a4d3e7f1ac6b4b6399cd2e8a9f528
SushantThakare <168189427+SushantThakare@users.noreply.github.com>
2024-08-16 10:57:47 +0530

    Company Library

---
+++ 38-CompanyLibrary/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _38_CompanyLibrary
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public int Experience { get; set; }
+
+        public Company Company { get; set; }
+
+        public void Details()
+        {
+            Console.WriteLine($"EmployeeId : {EmployeeId} Name : {Name} " +
+                $"City : {City} Experience : {Experience}");
+        }
+    }
+}
