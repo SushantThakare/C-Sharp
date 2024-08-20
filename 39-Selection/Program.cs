125d6020419d00ebc05712273bb29e5c0422b7e4
SushantThakare <168189427+SushantThakare@users.noreply.github.com>
2024-08-20 11:03:45 +0530

    Selection

---
+++ 39-Selection/Program.cs
@@ -0,0 +1,51 @@
+﻿using _39_StudentsSelection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _39_Selection
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Composition
+
+            //School s = new School()
+            //{
+            //    SchoolId = 1,
+            //    Name = "BPS",
+            //};
+            //s.Details();
+
+            //s.Company=new Company()
+            //{
+            //    CompanyId = 1,
+            //    Name="TCS"
+            //};
+
+            //s.Company.Details();
+
+            //Aggregation
+
+            //School s = new School()
+            //{
+            //    SchoolId = 1,
+            //    Name = "BPS",
+            //};
+            //Company c = new Company(s);
+            //c.CompanyPrint();
+
+            Students[] students=new Students[]
+            {
+                new Students(){RollNumber=1,Name="Sushnt"}
+            };
+            Teacher t = new Teacher(students);
+            t.StudentPrint();
+
+            Console.ReadLine();
+        }
+    }
+}
