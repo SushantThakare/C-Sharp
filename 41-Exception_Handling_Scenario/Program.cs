401fb8b6fbee3056460185c912575c271fcf7d64
SushantThakare <168189427+SushantThakare@users.noreply.github.com>
2024-08-24 14:01:45 +0530

    Exception Handling Scenario

---
+++ 41-Exception_Handling_Scenario/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _41_Exception_Handling_Scenario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = GetData();
+            //Console.WriteLine(name);
+            string name = GetData1();
+            Console.WriteLine(name);
+            Console.ReadLine();
+        }
+
+        static string GetData()
+        {
+            try
+            {
+                return "TRY";
+            }
+
+             catch
+            {
+                return "CATCH";
+            }
+            finally
+            {
+                Console.WriteLine("FINALLY");
+            }
+        }
+
+        static string GetData1()
+        {
+            string result = string.Empty;
+            try
+            {
+                result = "TRY";
+               // return result;
+            }
+
+            catch
+            {
+                result = "CATCH";
+                //return result;
+            }
+            finally
+            {
+                result = "FINALLY";
+                //Console.WriteLine("FINALLY");
+            }
+            return result;
+        }
+
+    }
+}
