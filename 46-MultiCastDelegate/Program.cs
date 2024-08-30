a8a2a4dade8a01bdac341b5174ebf4f7e01c04ed
SushantThakare <168189427+SushantThakare@users.noreply.github.com>
2024-08-30 14:23:11 +0530

    Multi Cast Delegate

---
+++ 46-MultiCastDelegate/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _46_MultiCastDelegate
+{
+    internal class Program
+    {
+        public delegate string PrintDelegate();
+        static void Main(string[] args)
+        {
+            PrintDelegate pd = PrintA;
+            pd += PrintB;
+            pd += PrintC;
+            //string name = pd();
+            //Console.WriteLine(name);
+
+            Delegate[] delegates = pd.GetInvocationList();
+            for (int i = 0; i < delegates.Length; i++)
+            {
+                try
+                {
+                    var name = delegates[i].DynamicInvoke();
+                    Console.WriteLine(name);
+                }
+                catch
+                {
+                    Console.WriteLine("Exception");
+                }
+            }
+            Console.ReadLine();
+        }
+
+        static string PrintA()
+        {
+            return "PrintA()";
+        }
+
+        static string PrintB()
+        {
+            throw new Exception("Intentionally exception thrown ");
+            return "PrintB()";
+        }
+
+        static string PrintC()
+        {
+            return "PrintC()";
+        }
+    }
+}
