57d05e33664d179229e475af73ae45ee547e21ee
SushantThakare <168189427+SushantThakare@users.noreply.github.com>
2024-09-05 15:31:38 +0530

    String And String Builder

---
+++ 50-StringAndStringBuilder/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _50_StringAndStringBuilder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string intro = "My ";
+            intro += "Name ";
+            intro += "Is ";
+            intro += "Sushant";
+            Console.WriteLine(intro);
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Hi ");
+            sb.Append("My self barik");
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
